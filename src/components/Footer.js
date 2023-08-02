241288bb13487d3d8e38dc7a43b710f9b2ec26d7
Joanna Patyk <patykjoanna93@gmail.com>
2023-08-02 12:31:52 +0200

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Wrapper from '../assets/wrappers/Footer';
+
+function Footer() {
+    return (
+        <Wrapper>
+            <div className="footer-container">
+                <p>Copyright © 2023 JP || All Rights Reserved</p>
+            </div>
+        </Wrapper>
+    );
+}
+
+export default Footer;
